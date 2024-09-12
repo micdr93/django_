576e76ae0699803db0131cfdabf289086c4639be
Michael Drew <michaeldrew_@outlook.com>
2024-09-12 17:24:24 +0000

    update

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!j(^^@ssgrn-ulbxlfw2s#5g3+0xu%1^!96#47#dx%oiemk7tc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-micdr93-django-uskcu37ryp4.ws-eu116.gitpod.io', 
 '.herokuapp.com']
